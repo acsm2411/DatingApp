44f283576f12630d8cdb442b182ec6e08c945757
Andres Silva <acsm2411@gmail.com>
2021-12-12 01:18:00 -0500

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
